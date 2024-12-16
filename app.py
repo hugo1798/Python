894dca8a03d955ffec8effe82d7748003392903c
hugosalcedo <hugo1798@hotmail.com>
2024-12-15 19:47:04 -0600

    HTML template finished with successful connection to data layer

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+from clienteDAO import ClienteDAO
+
 app = Flask(__name__)
 
 titulo_tab = 'Zona FIT, tu gimnasio'
@@ -9,11 +11,14 @@ titulo_app = 'Zona FIT'
 def inicio():
     app.logger.debug('Path de inicio')
     # Recuperando clientes de la base de datos
-    clientes_db = ClienteDAO.seleccionar()
+    clientes_db = ClienteDAO.listar_clientes()
+    print(clientes_db)
     return render_template('index.html',
                            titulo1=titulo_tab,
                            titulo2=titulo_app,
                            clientes=clientes_db)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
